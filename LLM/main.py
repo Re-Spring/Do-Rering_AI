b0235d80c57ab8d4ea10d2b35ab270f2c53cc393
Dongju Lee <dondjulee@gmail.com>
2024-02-26 13:07:32 +0900

    LLM first PR

---
+++ LLM/main.py
@@ -0,0 +1,138 @@
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+from openai import OpenAI
+from PIL import Image
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+import io
+import base64
+from fastapi.staticfiles import StaticFiles
+import httpx
+from fastapi.middleware.cors import CORSMiddleware
+# from typing import List
+from typing import Dict
+from pydantic import BaseModel
+
+load_dotenv()
+
+API_KEY = os.environ.get("OPENAI_API_KEY")
+
+client = OpenAI(
+    api_key = API_KEY
+)
+
+app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 실제 배포 시 특정 도메인으로 제한하는 것이 좋습니다.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# 이미지 프롬프트 리스트를 받는 스키마 정의
+# class ImagePrompt(BaseModel):
+#     prompts: List[str] # 여러 프롬프트를 리스트로 받음
+
+class ImagePrompt(BaseModel):
+    prompts: Dict[str, str]  # 키와 값 모두 문자열인 딕셔너리
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("LLM_test.html", {"request": request})
+
+@app.post("/mkimg")
+async def get_images(prompt: ImagePrompt):
+    print("get_image 들어옴")
+    image_paths = []
+
+    # 각 프롬프트에 대해 이미지 생성 및 경로 저장
+    for i, prompt_text in enumerate(prompt.prompts):
+        response = client.images.generate(
+            model="dall-e-3",
+            # 이미지 생성 prompt가 더 이미지를 잘 생성하도록 만들기
+            prompt=prompt_text+"라는 내용의 동화 페이지의 장면에 어울리는 그림을 동화 그림체로 만들어줘",
+            size="1024x1024",
+            quality="standard",
+            response_format='b64_json',
+            n=1,
+        )
+        image_data = base64.b64decode(response.data[0].b64_json)
+        print("image_data : ", image_data)
+        image = Image.open(io.BytesIO(image_data))
+        print("image : ", image)
+        image_path = f'static/LLM/images/output_{i}.png'
+        print("image_path : ", image_path)
+        image.save(image_path)
+        image_paths.append(image_path)
+
+    return JSONResponse({'image_paths': image_paths})
+
+
+
+
+# 여긴 스토리 창작 엔드포인트
+@app.post("/generate-story")
+async def generate_story():
+    api_key = API_KEY
+
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    
+    # 이 프롬프트는 동화 생성에 사용됩니다. 필요에 따라 수정 가능합니다.
+    payload = {
+        # option : gpt - streaming
+        "model": "gpt-4-turbo-preview",  # 또는 최신 GPT 모델을 사용하세요.
+        # "prompt" :
+        "messages": [
+        # role : user = 유저가 할 질문
+        # role : system = GPT의 역할 [ex)너는 동화 작가야] 
+        {
+            
+            "role": "system", "content":"너는 동화 스토리 작가야",
+            "role": "user", "content": "동화 스토리를 작성해줘."} 
+        ],
+        "max_tokens": 3000,  # 필요에 따라 토큰 수 조정
+        "temperature": 0.7,  # 창의성 정도 조정 0.7 ~ 1
+        "n": 1,  # 생성할 완료 항목 수
+    }
+
+    timeout = httpx.Timeout(600.0, connect=600.0) 
+    async with httpx.AsyncClient(timeout=timeout) as client:
+        response = await client.post('https://api.openai.com/v1/chat/completions', json=payload, headers=headers)
+        
+        # 상태 코드가 200이 아닐 경우
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.text)
+
+        result = response.json()
+        # print(result)
+        # if 'choices' in result and 'text' in result['choices']:
+        #     story_text = result['choices']['text'].strip()
+        # else:
+        #     print("응답 구조에서 'text' 정보를 찾을 수 없습니다.")
+
+        # 'choices' 배열에서 첫 번째 항목을 가져옵니다.
+        choice = result['choices'][0]  # 첫 번째 선택지를 가져옵니다.
+        # print(result)
+        # 'message' 객체 내의 'content' 필드에서 생성된 텍스트를 추출합니다.
+        if 'message' in choice and 'content' in choice['message']:
+            story_text = choice['message']['content'].strip()
+            # print("story_text : " + story_text)  # 생성된 텍스트 출력
+        else:
+            print("응답 구조에서 'content' 정보를 찾을 수 없습니다.")
+        # story_text = result['choices'][0]['text'].strip()
+        story_text = choice['message']['content'].replace("제목:","").strip()
+        paragraphs = story_text.split("\n\n")
+        story_dict = {f"paragraphs{i}": para for i, para in enumerate(paragraphs)}
+        print(story_dict)
+        return JSONResponse(content=story_dict)
---
+++ static/LLM/js/main.js
@@ -0,0 +1,77 @@
+// 웹 페이지의 모든 컨텐츠가 로드되고 준비된 후에 실행됩니다.
+document.addEventListener("DOMContentLoaded", function () {
+  // "storyForm"이라는 ID를 가진 폼이 제출될 때의 이벤트 리스너를 추가합니다.
+  document.getElementById("storyForm").addEventListener("submit", function (e) {
+    console.log("버튼 눌렀어용");
+    e.preventDefault(); // 폼의 기본 제출 동작을 막습니다. 즉, 페이지가 새로고침되지 않습니다.
+
+    var story = {};
+
+    // "/generate-story" 경로로 POST 요청을 보냅니다. 이는 서버에 이야기를 생성하도록 요청하는 것입니다.
+    fetch("/generate-story", {
+      method: "POST", // HTTP 메소드를 POST로 설정합니다.
+      headers: {
+        "Content-Type": "application/json", // 요청의 컨텐츠 타입을 JSON으로 지정합니다.
+      },
+    })
+      .then(console.log("fetch 나옴"))
+      .then((response) => response.json()) // 응답을 JSON 형태로 변환합니다.
+      .then((data) => {
+        const storyDiv = document.getElementById("storyOutput"); // 결과를 표시할 div 요소를 찾습니다.
+        storyDiv.innerHTML = ""; // 기존에 div 내에 있던 내용을 모두 지웁니다.
+
+        // 받아온 데이터의 각 키(key)를 순회하면서, 각 키에 해당하는 값을 담은 <p> 태그를 생성하고 div에 추가합니다.
+        Object.keys(data).forEach((key) => {
+          const para = document.createElement("p");
+          para.textContent = data[key]; // <p> 태그의 내용을 설정합니다.
+          storyDiv.appendChild(para); // 생성된 <p> 태그를 storyDiv에 추가합니다.
+          story[key] = data[key];
+          console.log("key 값 : ", key);
+          // console.log("value값 : ", story[key]);
+        });
+        console.log("story : " + story);
+        makePictures(story);
+        console.log(Object.keys(story).length);
+      })
+      .catch((error) => console.error("Error:", error)); // 오류가 발생하면 콘솔에 에러를 기록합니다.
+  });
+});
+
+function makePictures(story) {
+  console.log(story);
+  var input_text = story;
+  // console.log("input_text : " + input_text);
+  if (Object.keys(input_text).length > 0) {
+    console.log("fetch");
+    fetch("/mkimg", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompts: input_text,
+      }),
+    })
+      /* 지금 여기가 문제 */
+      .then((response) => {
+        console.log("then 1");
+        // console.log("response : " + response);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("then 2");
+        var outputImage = document.getElementById("output_image");
+        if (outputImage && data.image_path) {
+          outputImage.src = data.image_path + "?" + new Date().getTime();
+        }
+      })
+      .catch((error) => {
+        console.error("Fetch 동작 에러:", error);
+      });
+  } else {
+    console.log("story 없음");
+  }
+}
