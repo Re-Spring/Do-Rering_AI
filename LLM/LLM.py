d7edf6f0e600af8a254493e379584b892efa7cc5
Dongju Lee <dondjulee@gmail.com>
2024-02-28 17:09:24 +0900

    [Feature] prompt normalization

--- LLM/LLM.py
+++ LLM/LLM.py
@@ -65,11 +65,8 @@ async def get_images(prompt: ImagePrompt):
             n=1,
         )
         image_data = base64.b64decode(response.data[0].b64_json)
-        print("image_data : ", image_data)
         image = Image.open(io.BytesIO(image_data))
-        print("image : ", image)
         image_path = f'static/LLM/images/output_{i}.png'
-        print("image_path : ", image_path)
         image.save(image_path)
         image_paths.append(image_path)
 
@@ -102,6 +99,8 @@ async def generate_stoy(request: Request):
     page = data.get("page", "no request data")
     # story = data.get("story", "no requst data")
 
+    print(title, character, genre, keyword, lesson, page)
+
     api_key = API_KEY
 
     headers = {
@@ -119,43 +118,99 @@ async def generate_stoy(request: Request):
         # role : system = GPT의 역할 [ex)너는 동화 작가야] 
         {
             
-            "role": "system", "content":f"""
-    # Role
-    You are a fairy tale writer who creates fairy tales professionally and creatively.
+            "role": "system", "content":"""
+# Role
+You are a fairy tale writer who creates fairy tales professionally and creatively.
 
-    # Audience
-    - This is for Python developers who want to develop an automatic fairy tale generator.
-    - Do not write any content other than the json string, because the resulting json string must be used directly in the script.
+# Audience
+- This is for Python developers who want to develop an automatic fairy tale generator.
+- Do not write any content other than the json string, because the resulting json string must be used directly in the script.
 
-    # Task
-    The ultimate goal is to generate fairy tales by referring to the data sent by the user.
-    To do that, I'll think about each step.
 
-    ## Step 1
-    Set the settings for the fairy tale you receive by default.
-    The title of the fairy tale: {title},
-    The main character of the fairy tale: {character},
-    Genre of fairy tales: {genre},
-    Keyword of fairy tale: {keyword},
-    Lessons from fairy tales: {lesson},
-    Number of paragraphs in fairy tale: {page}
+# Output Format
+{
+"title" : "{title}",
+"story" : "{story}"
+}
 
-    If even one of the above setting values comes up with a value of no request data, go to Step 2,
-    Or move on to Step 3.
+# Task
+The ultimate goal is to generate fairy tales by referring to the data sent by the user.
+To do that, I'll think about each step.
+Please refer to "# Fairytale Attribute" for the six elements of fairy tales.
 
-    ## Step 2
-    Displays only the remaining elements except for settings with no request data.
+## Step 1
+In the process of writing fairy tales, we will refer to six elements of fairy tales.
+If there is a value of no request data among the six elements, please proceed to Step 2,
+If you don't have one, please go to Step 3.
 
-    ## Step 3
-    If you go directly from Step 1, you can set the fairy tale setting in Step 1,
-    If you have passed Step 2 instead of the above case, you should set the settings in Step 2
-    Create a fairy tale using
+## Step 2
+In the process of generating the fairy tale, please move to Step 3 on the condition that you do not think of the six elements of the fairy tale with the value of no request data.
 
-    # policy
-    - Please fill out the document type in the Json Format form below.
-    - Do not write any content other than the json string, because the resulting json string must be used directly in the script.
-    - Do not write unnecessary explanations or instructions.""",
-            "role": "user", "content": f"동화를 생성해줘"
+## Step 3
+- In this step, we will generate values for elements with values that are no request data based on the priorities of the elements of fairy tales.
+- First of all, the higher the priority, the higher the elements of assimilation with the value of no request data will be created first.
+- Create an element of fairy tale with the value of no request data by referring to the elements of fairy tale in order of priority, not the value of no request data.
+- However, for elements created in order in this case, the reference should be accompanied when the following elements are created.
+
+## Step 4
+- In this step, we will create a fairy tale.
+- All six elements of the fairy tale completed in Step 4 are referred to in priority.
+- Create a fairy tale based on the elements of the fairy tale you referenced.
+- However, the Number of paragraps in fairy tail, which determines the number of pages in a fairy tale, should never be different from the number of pages.
+
+
+# Fairytale Attribute
+## The main character of the fairy tale
+- It is an element that means the protagonist of a fairy tale.
+- It has the highest priority among the elements of fairy tales.
+- This element must be in the first line.
+
+## Keyword of fairy tale
+- It is an element that means the keyword that the user really wants to use.
+- It has the second priority among the elements of fairy tales.
+
+## The title of the fairy tale
+- It's an element that means the title of a fairy tale.
+- It has the third priority among the elements of fairy tales.
+
+## Lessons from fairy tales
+- It is an element that means the lessons that can be learned from the fairy tale.
+- It has the fourth priority among the elements of fairy tales.
+
+## Genre of fairy tales
+- It is an element that means the genre of the fairy tale.
+- It has the fifth priority among the elements of fairy tales.
+
+## Number of paragraphs in fairy tale
+- The element that means the number of pages in the fairy tale.
+- The corresponding element specifies the number of paragraphs excluding the title when creating the fairy tale.
+- It has the last priority among the elements of the fairy tale.
+- The priority is low, but it is a factor that must be followed.
+
+# policy
+- The resulting fairy tale should be in Korean.
+- Please fill out the document type in the Json Format form below.
+- Do not write any content other than the json string, because the resulting json string must be used directly in the script.
+- Do not write unnecessary explanations, instructions, or answers, but only print out the contents related to fairy tales.
+- When displaying a title, you should only display the title
+- Don't use words that tell you the structure or the end.
+    ex) The end, title : , ...
+- The number of pages generated by the Number of paragraps in fair tail property must be observed.
+""",
+            "role": "user", "content": f"""# Create a fairy tale based on the elements of the fairy tale.
+
+                ## The main character of the fairy tale : {character}
+                ## Keyword of fairy tale : {keyword}
+                ## The title of the fairy tale : {title}
+                ## Lessons from fairy tale : {lesson}
+                ## Genre of fairy tales : {genre}
+                ## Number of paragraphs in fairy tale : {page}
+
+                # request
+                ## Please print out the fairy tale in Korean           
+                ## Please print out the title on the first line
+"""
+                
             } 
         ],
         "max_tokens": 3000,  # 필요에 따라 토큰 수 조정
@@ -172,7 +227,8 @@ async def generate_stoy(request: Request):
             raise HTTPException(status_code=response.status_code, detail=response.text)
 
         result = response.json()
-        # print(result)
+        print("result")
+        print(result)
         # if 'choices' in result and 'text' in result['choices']:
         #     story_text = result['choices']['text'].strip()
         # else:
@@ -180,6 +236,9 @@ async def generate_stoy(request: Request):
 
         # 'choices' 배열에서 첫 번째 항목을 가져옵니다.
         choice = result['choices'][0]  # 첫 번째 선택지를 가져옵니다.
+        print("choices")
+        print(choice)
+   
         # print(result)
         # 'message' 객체 내의 'content' 필드에서 생성된 텍스트를 추출합니다.
         if 'message' in choice and 'content' in choice['message']:
@@ -188,8 +247,9 @@ async def generate_stoy(request: Request):
         else:
             print("응답 구조에서 'content' 정보를 찾을 수 없습니다.")
         # story_text = result['choices'][0]['text'].strip()
-        story_text = choice['message']['content'].replace("제목:","").strip()
+        story_text = choice['message']['content'].replace("*","").replace("#","").replace("제목:","").strip()
         paragraphs = story_text.split("\n\n")
         story_dict = {f"paragraphs{i}": para for i, para in enumerate(paragraphs)}
+        print("story_dict")
         print(story_dict)
         return JSONResponse(content=story_dict)
